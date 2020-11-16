32f0fa3f7ae42cf61d162f88d4a5fa4fbf27a6a6
Fumi Hin <fumihin@outlook.com>
2020-11-16 17:11:57 +0300

    Input auto height resize

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,11 +3,34 @@ import React from "react";
 import "./Input.css";
 
 const Input = () => {
+
+  const resize = () => {
+    const textarea = document.getElementById("input");
+
+    var minRows = textarea.getAttribute('data-min-rows')|0, rows;
+    !textarea._baseScrollHeight && getScrollHeight(textarea);
+
+    textarea.rows = minRows;
+    rows = Math.ceil((textarea.scrollHeight - textarea._baseScrollHeight) / 34);
+    textarea.rows = minRows + rows;
+  };
+
+  const getScrollHeight = (textarea) => {
+    var savedValue = textarea.value;
+    textarea.value = '';
+    textarea._baseScrollHeight = textarea.scrollHeight;
+    textarea.value = savedValue;
+    console.log(textarea._baseScrollHeight);
+  };
+
   return (
     <form>
 
       <textarea
+        id="input"
+        data-min-rows="1"
         rows="1"
+        onKeyUp={resize}
       ></textarea>
 
       <button type="submit"
@@ -15,6 +38,7 @@ const Input = () => {
 
     </form>
   );
+
 };
 
 export default Input;
