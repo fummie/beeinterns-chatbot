0cb4ccc97be41aa138c232598a2c9094c3f60520
Fumi Hin <fumihin@outlook.com>
2020-11-17 10:55:48 +0300

    Text input in separated component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,42 +1,16 @@
 import React from "react";
 
+import Text from "../Text";
+
 import "./Input.css";
 
 const Input = () => {
-
-  const resize = () => {
-    const textarea = document.getElementById("input");
-
-    var minRows = textarea.getAttribute('data-min-rows')|0, rows;
-    !textarea._baseScrollHeight && getScrollHeight(textarea);
-
-    textarea.rows = minRows;
-    rows = Math.ceil((textarea.scrollHeight - textarea._baseScrollHeight) / 34);
-    textarea.rows = minRows + rows;
-  };
-
-  const getScrollHeight = (textarea) => {
-    var savedValue = textarea.value;
-    textarea.value = '';
-    textarea._baseScrollHeight = textarea.scrollHeight;
-    textarea.value = savedValue;
-    console.log(textarea._baseScrollHeight);
-  };
-
   return (
-    <form>
-
-      <textarea
-        id="input"
-        data-min-rows="1"
-        rows="1"
-        onKeyUp={resize}
-      ></textarea>
-
-      <button type="submit"
-      ></button>
-
-    </form>
+    <div className="input">
+      <form>
+        <Text/>
+      </form>
+    </div>
   );
 
 };
