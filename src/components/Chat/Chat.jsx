a02f0f3e3f49ebb1cf48abe89e746879f5c1e124
Fumi Hin <fumihin@outlook.com>
2020-11-17 15:33:13 +0300

    Message creating functions

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }  from "react";
 
 import Messages from "../Messages";
 import Input from "../Input";
@@ -6,9 +6,42 @@ import Input from "../Input";
 import "./Chat.css";
 
 const Chat = () => {
+  const [id, setId] = useState(0);
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = (author, text) => {
+    const message = createMessage(author, text);
+    const newMessages = messages;
+    newMessages.push(message);
+    setMessages(newMessages);
+
+    if (author != "Bot")
+      createRespond(message);
+  };
+
+  const addRespond = (message) => {
+    const author = "Bot";
+    const text = "Your request is:" + message.text;
+    const message = createMessage(author, text);
+    addMessage(message);
+  };
+
+  const createMessage = (author, text) => {
+    const savedId = id;
+    setId(id + 1);
+    return {
+      id: savedId,
+      author,
+      text
+    };
+  };
+
   return (
     <div className="chat">
-      <Messages/>
+      <Messages
+        messages={messages}
+        addMessage={addMessage}
+      />
       <Input/>
     </div>
   );
