8c6fd12d38bef70c00ee32b0c74fa993eded2e1f
Fumi Hin <fumihin@outlook.com>
2020-11-18 19:52:27 +0300

    Added icons with message bubble

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,13 +2,33 @@ import React from "react";
 
 import "./Message.css";
 
+import botSVG from "../../data/bot.svg";
+import userSVG from "../../data/user.svg";
+
 const Message = ({ author, text }) => {
+
+  let icon = null;
+  let bubble = null;
+
+  if (author === "Bot") {
+    bubble = "bot";
+    icon = botSVG;
+  } else {
+    bubble = "user";
+    icon = userSVG;
+  };
+
   return (
-    <span>
-      <p>Author: {author}</p>
-      <p>Text: {text}</p>
-    </span>
+    <div>
+      <div className="icon">
+        <img src={icon} alt=""></img>
+      </div>
+      <div className={bubble}>
+        <p>{text}</p>
+      </div>
+    </div>
   );
+
 };
 
 export default Message;
